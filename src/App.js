a3470bb8fe533a7cc1791d8b28b56c32caa12ac8
Irvin Fiz <irvin.fiz.dev@gmail.com>
2020-05-08 13:19:24 -0500

    layout app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import {BrowserRouter, Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class App extends Component {
+  state={
+    server: 'https://warehouse-stock-server.irvinfiz.now.sh/graph',
+    suppliers: []
+
+  }
+
+  async componentDidMount(){
+    const query = `query{
+      suppliers {
+        _id
+      }
+    }`
+
+    const url = this.state.server;
+    const opts = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ query })
+    };
+    const res = await fetch(url, opts);
+    const data = await res.json();
+    console.log(data.data.suppliers)
+    this.setState({suppliers: data.data.suppliers })
+  }
+
+
+  render(){
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <div className="Content">
+            hola
+            <Switch>
+
+              {/* <Route path="/education/certifications/:id" exact component={ props => ( <Certification {...props} 
+              paths={this.state.paths} /> )} 
+              /> */}
+            </Switch>
+          </div>
+          <div className='Options'>
+           
+          </div>
+          <div className='Footer'>
+           
+          </div>
+        </div>
+      
+      </BrowserRouter>
   );
+  }
 }
 
 export default App;
